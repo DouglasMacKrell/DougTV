41cfe42f31c92d91287c9c5ae533fb1c124189d7
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-06-05 21:46:06 -0400

    path fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 
 app.use(express.static(__dirname + "client/build"));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
