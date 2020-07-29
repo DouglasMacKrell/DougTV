bda80539aab40d7c0a0f6245b6e84e5ae8a5d6b2
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-07-28 20:01:43 -0400

    watch page background adjust

--- client/src/components/useUserMedia/useUserMedia.jsx
+++ client/src/components/useUserMedia/useUserMedia.jsx
@@ -1,32 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
-const useUserMedia = (requestedMedia) => {
+const useUserMedia = requestedMedia => {
   const [mediaStream, setMediaStream] = useState(null);
 
   useEffect(() => {
     const enableStream = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia(
-          requestedMedia
-        );
+        const stream = await navigator.mediaDevices.getUserMedia(requestedMedia);
         setMediaStream(stream);
-      } catch (err) {
-        console.log(err);
+      } catch(err) {
+        console.log(err)
       }
-    };
+    }
 
     if (!mediaStream) {
       enableStream();
     } else {
       return function cleanup() {
-        mediaStream.getTracks().forEach((track) => {
+        mediaStream.getTracks().forEach(track => {
           track.stop();
         });
-      };
+      }
     }
   }, [mediaStream, requestedMedia]);
 
   return mediaStream;
-};
+}
 
-export default useUserMedia;
+export default useUserMedia
