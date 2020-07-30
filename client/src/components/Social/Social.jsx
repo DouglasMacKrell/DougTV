8b9f80ef89dfe727af5eb738fd098c14597690b2
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-07-30 18:23:04 -0400

    social component fix

--- client/src/components/Social/Social.jsx
+++ client/src/components/Social/Social.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SocialImg from "../../../public/DougTV-Social.png";
+import SocialImg from "./DougTV-Social.png";
 
 const Social = () => {
   return <img src={SocialImg} alt="DougTV Social large image" />;
