4912fd8de8ae643be5e6b559d16568af3ce1d0d0
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-06 13:20:45 -0400

    Social img reapir step 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Join from "./components/Join/Join";
 import Broadcast from "./components/Broadcast/Broadcast";
 import Watch from "./components/Watch/Watch";
-import Social from "./components/Social/Social"
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact component={Join} />
-      <Route path="/broadcast" component={Broadcast} />
-      <Route path="/watch/:broadcasterId" component={Watch} />
-      <Route path="/social" component={Social} />
+      <Switch>
+        <Route path="/" exact component={Join} />
+        <Route path="/broadcast" component={Broadcast} />
+        <Route path="/watch/:broadcasterId" component={Watch} />
+      </Switch>
     </Router>
   );
 };
--- client/src/components/Social/Social.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SocialImg from "./DougTV-Social.png";
-
-const Social = () => {
-  return <img src={SocialImg} alt="DougTV Social large image" />;
-};
-
-export default Social;
